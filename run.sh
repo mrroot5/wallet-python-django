508bda2abde978cfec6d08f4d45fbe1075560214
Adrian Garrido <agarrido@qdqmedia.com>
2021-10-16 11:22:55 +0200

    Removed unnecessary files

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Activate en
-. venv/bin/activate
-# Migrations
-python3 manage.py makemigrations api
-python3 manage.py migrate
-# Start server
-python3 manage.py runserver 0.0.0.0:8000
