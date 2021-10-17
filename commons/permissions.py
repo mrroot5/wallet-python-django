d3eda2dff782f932c05c478305c8f1d4153e4a2a
Adrian Garrido <agarrido@qdqmedia.com>
2021-10-17 20:44:57 +0200

    Permissions file added

---
+++ commons/permissions.py
@@ -0,0 +1,34 @@
+from rest_framework import permissions
+
+
+class AnonCreateAndUpdateOwnerOnly(permissions.BasePermission):
+    """
+    Custom permission:
+        - allow anonymous POST
+        - allow authenticated GET and PUT on *own* record
+        - allow all actions for staff
+    """
+
+    def has_permission(self, request, view):
+        return view.action == 'create' or request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return view.action in ['retrieve', 'update', 'partial_update'] and obj.id == request.user.id or request.user.is_staff
+
+
+class ListStaffOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow access to lists for admins
+    """
+
+    def has_permission(self, request, view):
+        return view.action != 'list' or request.user and request.user.is_staff
+
+
+class IsStaff(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        try:
+            return request.user.is_staff
+        except (AttributeError, TypeError):
+            return False
